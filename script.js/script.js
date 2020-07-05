3a8e644992347a884bceebdf6b702c1f770d85a9
Valeriya Zolotaryova <Panda.lera@mail.ru>
2020-07-05 11:12:24 +0300

    fixed all bags

---
+++ script.js/script.js
@@ -0,0 +1,36 @@
+const editButton = document.querySelector(".intro__edit-button");
+const popup = document.querySelector(".popup");
+const popupClose = document.querySelector(".popup__button");
+const popupOpened = document.querySelector(".popup");
+const formElement = document.querySelector(".popup__form");
+const nameInput = formElement.querySelector(".popup__input_name");
+const jobInput = formElement.querySelector(".popup__input_job");
+const intro = document.querySelector(".intro");
+const introTitle = intro.querySelector(".intro__title");
+const introSubTitle = intro.querySelector(".intro__subtitle");
+
+function showClick() {
+  popupOpened.classList.add("popup_opened");
+  introTitle.textContent = nameInput.value;
+  introSubTitle.textContent = jobInput.value;
+}
+
+function closeForm() {
+  popupOpened.classList.remove("popup_opened");
+  formElement.reset();
+}
+function formElementSubmitHandler(evt) {
+  evt.preventDefault(); 
+  introTitle.textContent = nameInput.value;
+  introSubTitle.textContent = jobInput.value;
+}
+formElement.addEventListener("submit", formElementSubmitHandler);
+editButton.addEventListener("click", showClick);
+popupClose.addEventListener("click", closeForm);
+
+
+/*function toggleclick() {
+    popupOpened.classList.toggle('popup_opened');
+}
+editButton.addEventListener("click", toggleclick);
+popupClose.addEventListener("click", toggleclick)*/
