3ac0f36939c0388e25996d2572c56eb011453934
Valeriya Zolotaryova <Panda.lera@mail.ru>
2020-08-24 08:52:27 +0500

    Revert "add new class to create 6 cards"
    This reverts commit 319485d57612aaba0cd56c59dfcd8261c2324498.

--- script/index.js
+++ script/index.js
@@ -107,186 +107,6 @@ popupCloseCards.addEventListener('click', function() {
 closeForm(popupOpenedCards);
 }); 
 
-
-//закрытие модалки по клавише escape
-const closePopupEscape = (form) => {
- document.addEventListener('keydown', (evt) => {
-  if(evt.key === 'Escape') {
-    closeForm(form);
-  }
- }); 
-};
-
-//закрытие модалки по клику на фон
-const closePopupOnBackgroundClick = (form) => {
-  document.addEventListener('click', (evt) => {
-   if(evt.target.classList.contains('popup__background')) {
-     closeForm(form);
-   }
-  }); 
-};
-
-formElement.addEventListener("submit",formElementSubmitHandler);
-function formElementSubmitHandler(evt) { 
- evt.preventDefault();  
-  introTitle.textContent = nameInput.value; 
-  introSubTitle.textContent = jobInput.value; 
-  closeForm(popupOpened); 
-} 
-
-
-//класс кард
-class Card {
-  constructor(data, cardSelector){
-    this._link = data.link;
-    this._name = data.name;
-    this._cardSelector = cardSelector;
-  }
-  //метод возвращающий разметку карточки
-  _getTemplate(){
-    const cardElement = document.querySelector(this._cardSelector)
-    .content.querySelector('.elements__item')
-    .cloneNode(true);
-    return cardElement;
-  }
-
-  //публчиный метод генерит карточки
-  generateCard(){
-    this._element = this._getTemplate();
-    this._setEventListeners();
-    this._element.querySelector('.elements__item-img').src = this._link;
-    this._element.querySelector('.elements__text-title').textContent = this._name;
-    this._element.querySelector('.elements__text-like');
-    this._element.querySelector('.elements__delete');
-    return this._element;
-  }
-  //слушатели событий 
-  _setEventListeners(){
-    //слушает кнопку лайка дизлайка
-    this._element.querySelector('.elements__text-like').addEventListener('click', () => {
-      this._handleLikeClick();
-    });
-    //слушает кнопку удаления карты с фото
-    this._element.querySelector('.elements__delete').addEventListener('click', () => {
-      this._handleDeleteCard();
-    });
-    //слушает клик по фото и открывает попап с фото
-    this._element.querySelector('.elements__item-img').addEventListener('click', () => {
-      this._showPhotoClick();
-    });
-    //слушает клик по крестику для закрытия попапа
-    popupClosePhoto.addEventListener('click', () => {
-      this._closePopupCross();
-    });
-  }
-  //методы обработки карточек
-  //метод обработки лайка дизлайка
-  _handleLikeClick = () => {
-    this._element.querySelector('.elements__text-like').classList.toggle('elements__text-like_active');
-  }
-  //метод обработки удаления картчоки с фото
-  _handleDeleteCard = () => {
-    this._element.querySelector('.elements__delete').closest('.elements__item').remove();
-  }
-  //метод открытия попапа с фотографией
-  _showPhotoPopup = () => {
-    photoPopup.classList.add('popup_opened');
-  }
-  //метод закрытия попапа с фото
-  _closePhotoPopup = () => {
-    photoPopup.classList.remove('popup_opened')
-  }
-  //метод закрытия попапа с фото по клавише эскейп
-  _closePopupEscape =() => {
-    document.addEventListener('keydown', (evt) => {
-      if(evt.key === 'Escape') {
-        this._closePhotoPopup();
-      }
-    });
-  }
-  //метод закрытия попапа по клику на фон
-  _closePopupOnBackgroundClick = () => {
-    document.addEventListener('click', (evt) => {
-      if(evt.target.classList.contains('popup__background')) {
-        this._closePhotoPopup();
-      }
-     });
-  }
-  //метод закрытия попапа по крестику
-  _closePopupCross = () => {
-    this._closePhotoPopup();
-  }
-  // метод открытия попаа с фото
-  _showPhotoClick = () => {
-    photoTitle.textContent =  this._name;
-    photoImage.src = this._link;
-    photoImage.alt = this._name;
-    this._showPhotoPopup();
-    this._closePopupEscape(); 
-    this._closePopupOnBackgroundClick();   
-  }
-}
-//добавляем новую карточку через форму
-submitButtonCardPhoto.addEventListener('click', addCardSubmitHandler = (evt)  => {
-    const cards = new Card({name:cardName.value,link:cardLink.value}, '.cards');
-    const cardEl = cards.generateCard(cardName.value,cardLink.value);
-    evt.preventDefault();
-    elements.prepend(cardEl);
-    closeForm(popupOpenedCards);
-    submitButtonCardPhoto.setAttribute('disabled','disabled');
-    submitButtonCardPhoto.classList.add('popup__submit_inactive');
-});
-
-//проходимся по массиву и рендерим 6 карточек на страницу
-initialCards.forEach((item) => {
-    const card = new Card(item, '.cards');
-    const cardElement = card.generateCard();
-    elements.prepend(cardElement);
-});
-
-
-
-
-/*
-//функция открытия модалок.
-function showClick(form) {
-  form.classList.add('popup_opened');
-}
-
-//функция закрытия модалок
-function closeForm(form) {
-  form.classList.remove('popup_opened'); 
-  formElement.reset();
-  formElementAddCard.reset();
-  document.removeEventListener('keydown', closePopupEscape);
-  document.removeEventListener('click',closePopupOnBackgroundClick);
-} 
-
-//слушатели событий
-editButton.addEventListener('click', function(){
-  showClick(popupOpened); 
-  nameInput.value = introTitle.textContent; 
-  jobInput.value = introSubTitle.textContent;
-  closePopupEscape(popupOpened);
-  closePopupOnBackgroundClick(popupOpened);
-});
-
-addButton.addEventListener('click', () => {
-  showClick(popupOpenedCards);
-  closePopupEscape(popupOpenedCards);
-  closePopupOnBackgroundClick(popupOpenedCards);
-});
-
-//слушатель закрытия попапа редактирования формы
-popupClose.addEventListener('click', function() {
-closeForm(popupOpened);
-}); 
-
-//слушатель закрытия попапа добавления карточки
-popupCloseCards.addEventListener('click', function() {
-closeForm(popupOpenedCards);
-}); 
-
 //слушатель закрытия попапа с фотогарфией
 popupClosePhoto.addEventListener('click', function() {
   closeForm(photoPopup);
@@ -321,7 +141,6 @@ function formElementSubmitHandler(evt) {
   closeForm(popupOpened); 
 } 
 
-
 function createCard(data) {
   const cardsElement = cardsTemplate.cloneNode(true);
   const titleImage = cardsElement.querySelector('.elements__item-img');
@@ -351,7 +170,7 @@ function createCard(data) {
 
   return cardsElement;
 }
-  
+
 function renderCard(data) {
   elements.prepend(createCard(data))
 }
@@ -368,7 +187,7 @@ function addCardSubmitHandler(evt) {
   submitButtonCardPhoto.classList.add('popup__submit_inactive');
 }
 
-*/
+
 
 
 
--- script/validate.js
+++ script/validate.js
@@ -11,98 +11,8 @@ const object = {
   errorClass: 'popup__input-error_visible'
 }
 
-class FormValidator{
-  
-  constructor(object, formElement){
-    this._formElement = formElement;
-    this._object = object;
-    /*this._formSelector = object.formSelector;
-    this._inputSelector = object.inputSelector;
-    this._submitButtonSelector = object.submitButtonSelector;
-    this._activeButtonClass = object.activeButtonClass;
-    this._inactiveButtonClass = object.inactiveButtonClass;
-    this._inputErrorClass= object.inputErrorClass;
-    this._inputValidClass= object.inputValidClass;
-    this._errorElement = object.errorElement;
-    this._errorClass = object.errorClass;*/
-  }
-  
-  _showInputError (errorMessage) {
-    const inputElement = this._formElement.querySelector('.popup__input');
-    const errorElement = this._formElement.querySelector(`#${inputElement.name}-error`);
-    inputElement.classList.add(this._object.inputErrorClass);
-    errorElement.textContent = errorMessage;
-    errorElement.classList.add(this._object.errorClass);
-  }
-
-  _hideInputError () {
-    const inputElement = this._formElement.querySelector('.popup__input');
-    const errorElement = this._formElement.querySelector(`#${inputElement.name}-error`);
-    inputElement.classList.remove(this._object.inputErrorClass);
-    errorElement.textContent = '';
-    errorElement.classList.remove(this._object.errorClass);
-  }
-
-  _isValid () {
-    const inputElement = this._formElement.querySelector('.popup__input');
-    if(!inputElement.validity.valid){
-      this._showInputError(inputElement.validationMessage);
-    }else{
-      this._hideInputError();
-    } 
-  }
-
-  _hasInvalidInput () {
-    const inputs = Array.from(this._formElement.querySelectorAll(this._object.inputSelector));
-    return inputs.some((inputElement) => {
-    return !inputElement.validity.valid;
-    })
-  }
-
-  _toggleButtonState () {
-    const inputs = Array.from(this._formElement.querySelectorAll(this._object.inputSelector));
-    const buttonSubmit = this._formElement.querySelector(this._object.submitButtonSelector);
-    if (this._hasInvalidInput(inputs)) {
-      buttonSubmit.classList.add(this._object.inactiveButtonClass);
-      buttonSubmit.disabled = true;
-    } else {
-      buttonSubmit.classList.remove(this._object.inactiveButtonClass);
-      buttonSubmit.disabled = false;
-    }
-  }
-
-  _setEventListeners () {
-  const inputs = Array.from(this._formElement.querySelectorAll(this._object.inputSelector));
-  this._toggleButtonState();
-  inputs.forEach((inputElement) => {
-    inputElement.addEventListener('input', (evt) => {
-      this._isValid();
-      this._toggleButtonState();
-    });
-  })
-  }
-
-  enableValidation(){
-    const forms = Array.from(document.querySelectorAll(this._object.formSelector));
-    forms.forEach((formElement) => {
-      formElement.addEventListener('submit', (evt) => {
-      evt.preventDefault();
-    }); 
-
-    this._setEventListeners();
-  });
-  }
 
 
-}
-
-
-const formEl = document.querySelector("#popup-editForm");
-const form = new FormValidator(object, formEl);
-const formValidation = form.enableValidation();
-
-
-/*
 const setEventListeners = (formElement, inputSelector, submitButtonSelector, inactiveButtonClass, 
   inputErrorClass, errorClass ) => {
   // Находим все поля внутри формы, сделаем из них массив методом Array.from
@@ -176,4 +86,3 @@ function enableValidation({ formSelector, inputSelector, submitButtonSelector,ac
 };
 enableValidation(object);
 
-*/
